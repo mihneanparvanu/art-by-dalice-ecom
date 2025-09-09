03ae0451ccd8ba4cc547b1a4fbe5f3b9f58c8eb9
Mihnea <m@mihneaparvanu.ro>
2025-09-10 01:56:22 +0300

    Navbar fixes

--- src/components/layout/DaliceNavbar.tsx
+++ src/components/layout/DaliceNavbar.tsx
@@ -1,33 +1,59 @@
-import { css } from "@linaria/core"
-import { styled } from "@linaria/react"
-import DaliceLogo from "../../../public/icons/DaliceLogo"
+import { css } from "@linaria/core";
+import { styled } from "@linaria/react";
+import DaliceLogo from "../../../public/icons/DaliceLogo";
+import { LucideCircleUser } from "lucide-react";
+import Link from "next/link";
 
 const NavContainer = styled.div`
   display: flex;
-justify-content: start;
-width: 100%;
-height: fit-content;
-padding-block: 0.5rem;
-padding-inline: 2rem;
-background-color: var(--accent-primary);
-`
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: fit-content;
+  padding-block: 0.5rem;
+  padding-inline: 2rem;
+  background-color: var(--accent-primary);
+`;
 
-export default function DaliceNavbar () {
-  return <NavContainer>
-    <div className={logoContainer}>
-      <DaliceLogo fill="#ffffff" 
-      className={logo}></DaliceLogo>
-    </div>
-</NavContainer>
+export default function DaliceNavbar() {
+  return (
+    <NavContainer>
+      <div className={logoContainer}>
+        <DaliceLogo fill="#ffffff" className={logo}></DaliceLogo>
+      </div>
+      <div className={menu}>
+        <Link href="">Shop</Link>
+        <Link href="">Discover</Link>
+        <Link href="">Galleries</Link>
+        <Link href="">Contact</Link>
+      </div>
+      <div>
+        <div></div>
+        <div>
+          <LucideCircleUser></LucideCircleUser>
+        </div>
+      </div>
+    </NavContainer>
+  );
 }
 
 // Styles
 
 const logoContainer = css`
-height: 96px;
-`
+  height: 5rem;
+`;
 
 const logo = css`
-width: 100%;
-height: 100%;
-`
+  width: 100%;
+  height: 100%;
+`;
+
+const menu = css`
+  position: absolute;
+  font-size: 1rem;
+  font-weight: 400;
+  letter-spacing: 4%;
+  text-transform: uppercase;
+  display: flex;
+  gap: 1rem;
+`;
