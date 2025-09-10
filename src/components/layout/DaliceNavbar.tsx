81ffc052cfd5e41b8324b79df83dd41b5a9e0275
Mihnea <m@mihneaparvanu.ro>
2025-09-10 03:24:34 +0300

    Desktop navbar done

--- src/components/layout/DaliceNavbar.tsx
+++ src/components/layout/DaliceNavbar.tsx
@@ -4,6 +4,28 @@ import DaliceLogo from "../../../public/icons/DaliceLogo";
 import { LucideCircleUser } from "lucide-react";
 import Link from "next/link";
 
+export default function DaliceNavbar() {
+  return (
+    <NavContainer>
+      <InnerContainer className="container">
+        <LogoContainer>
+          <DaliceLogo fill="var(--white)" className={Logo}></DaliceLogo>
+        </LogoContainer>
+        <Menu>
+          <Link href="">Shop</Link>
+          <Link href="">Discover</Link>
+          <Link href="">Galleries</Link>
+          <Link href="">Contact</Link>
+        </Menu>
+        <IconsContainer>
+          <LucideCircleUser></LucideCircleUser>
+        </IconsContainer>
+      </InnerContainer>
+    </NavContainer>
+  );
+}
+
+// Styles
 const NavContainer = styled.div`
   background-color: var(--accent-primary);
   display: flex;
@@ -31,35 +53,16 @@ const Menu = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
-  gap: 2rem;
+  gap: 1.8rem;
+  font-size: 0.9rem;
   text-transform: uppercase;
   font-weight: 400;
   letter-spacing: 4%;
 `;
+
 const IconsContainer = styled.div`
   display: flex;
-`;
-
-export default function DaliceNavbar() {
-  return (
-    <NavContainer>
-      <InnerContainer className="container">
-        <LogoContainer>
-          <DaliceLogo fill="#ffffff" className={Logo}></DaliceLogo>
-        </LogoContainer>
-        <Menu>
-          <Link href="">Shop</Link>
-          <Link href="">Discover</Link>
-          <Link href="">Galleries</Link>
-          <Link href="">Contact</Link>
-        </Menu>
-        <IconsContainer>
-          <LucideCircleUser></LucideCircleUser>
-        </IconsContainer>
-      </InnerContainer>
-    </NavContainer>
-  );
-}
+`
 
 const Logo = css`
   width: 100%;
