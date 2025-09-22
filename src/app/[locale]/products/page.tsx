23a300cca6f06f1fc0eb5521ddcd84bd7f45bc89
Mihnea <m@mihneaparvanu.ro>
2025-09-23 02:30:31 +0300

    Improved products page

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -1,6 +1,9 @@
 import fetchProducts from "../../../data/FetchProducts";
 import ProductCard from "../../../components/product/ProductCard";
 import Link from "next/link";
+
+import { styled } from "@linaria/react";
+
 export default async function ProductsPage(props: {
   searchParams: Promise<{ album: string }>;
   params: Promise<{ locale: string }>;
@@ -8,28 +11,42 @@ export default async function ProductsPage(props: {
   const album = (await props.searchParams).album;
   const params = await props.params;
   const locale = params.locale || "en";
+
   const products = await fetchProducts(locale, album);
+  const hasProducts = Array.isArray(products) && products.length > 0;
+
+  const NoProductsDisplay = styled.div`
+    height: 80dvh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.25rem;
+    font-weight: 600;
+    color: var(--content-tertiary);
+  `;
+
+  if (!hasProducts) {
+    return <NoProductsDisplay>No products found</NoProductsDisplay>;
+  }
+
   return (
-    <main className="flex py-10 px-3">
-      <div className="flex flex-col items-center w-full">
+    <main className="flex py-10 px-3 justify-center">
+      <div className="flex flex-col justify-center">
         <div className="grid gap-10 grid-cols-2 lg:grid-cols-4">
-          {products &&
-            products.map((product) => (
-              <Link
-                href={{ pathname: "/product", query: { id: product.id } }}
-                key={product.id}
-              >
-                <ProductCard
-                  productTitle={product.name}
-                  productPrice={product.price}
-                  productImageURL={product.image_url}
-                />
-              </Link>
-            ))}
+          {products.map((product) => (
+            <Link
+              href={{ pathname: "/product", query: { id: product.id } }}
+              key={product.id}
+            >
+              <ProductCard
+                productTitle={product.name}
+                productPrice={product.price}
+                productImageURL={product.image_url}
+              />
+            </Link>
+          ))}
         </div>
       </div>
     </main>
   );
 }
-
-
