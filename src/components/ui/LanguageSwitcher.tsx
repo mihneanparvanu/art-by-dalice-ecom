7010d43d7cf3c77123601c411c38b1542db3a54b
Mihnea <m@mihneaparvanu.ro>
2025-09-23 15:23:09 +0300

    Working on language switcher

--- src/components/ui/LanguageSwitcher.tsx
+++ src/components/ui/LanguageSwitcher.tsx
@@ -3,7 +3,10 @@
 
 import { useRouter, usePathname } from "../../i8n/navigation";
 import { useLocale } from "next-intl";
+
 import { styled } from "@linaria/react";
+
+import RoFlag from "../icons/RoFlag";
 
 export default function LanguageSwitcher() {
   const router = useRouter();
@@ -16,7 +19,9 @@ export default function LanguageSwitcher() {
   }
 
   const otherLocale = currentLocale === "en" ? "ro" : "en";
+
   const dropdownOption = otherLocale === "en" ? "English" : "Romanian";
+  const dropdownFlag = otherLocale === "en" ? <EnFlag /> : <RoFlag />;
 
   // Styles
   const Container = styled.div`
@@ -27,17 +32,33 @@ export default function LanguageSwitcher() {
   `;
 
   const Button = styled.button`
-    background: var(--accent-primary, #000);
+    background: var(--accent-primary);
     color: var(--white, #fff);
-    border: none;
+    border-radius: calc(infinity * 1px);
     padding: 0.5rem 1rem;
-    border-radius: 0.3rem;
     cursor: pointer;
     font-size: 1rem;
     text-transform: uppercase;
 
     &:hover {
-      background: var(--accent-secondary, #333);
+      background: var(--accent-secondary);
+    }
+  `;
+
+  const FlagContainer = styled.div`
+    width: 4rem;
+    height: 4rem;
+    border-radius: calc(infinity * 1px);
+    position: relative;
+    overflow: hidden;
+
+    #flag {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: auto;
     }
   `;
 
@@ -47,7 +68,7 @@ export default function LanguageSwitcher() {
         onClick={() => switchTo(otherLocale)}
         className="languageButton languageSwitcherButton"
       >
-        {dropdownOption}
+        <FlagContainer> {dropdownFlag} </FlagContainer>
       </Button>
     </Container>
   );
