391fe1688c466f707babb2b86f9bb47c85c74931
Mihnea <m@mihneaparvanu.ro>
2025-09-30 23:36:41 +0300

    Basic template for pages implemented with page title

---
+++ src/app/[locale]/content-page/page.tsx
@@ -0,0 +1,5 @@
+import ContentPage from "@/components/layout/ContentPage";
+
+export default function ContentTest() {
+  return <ContentPage pageTitle="Warranty & Returns"></ContentPage>;
+}
--- src/components/layout/ContentPage.tsx
+++ src/components/layout/ContentPage.tsx
@@ -1,9 +1,30 @@
 import { styled } from "@linaria/react";
 
-const Container = styled.div`
-  padding-inline: 1rem;
+const TitleContainer = styled.div`
+  font-size: 2rem;
 `;
 
-export default function ContentPage() {
-  return <Container> Content </Container>;
+const Page = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  width: 85%;
+  max-width: 90%;
+`;
+
+export default function ContentPage({ pageTitle }: { pageTitle: string }) {
+  return (
+    <Page>
+      <Container>
+        {" "}
+        <TitleContainer>
+          <h1> {pageTitle} </h1>
+        </TitleContainer>
+      </Container>
+    </Page>
+  );
 }
